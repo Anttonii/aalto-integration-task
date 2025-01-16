fdb93ba0151594ebc4df4b7dac4125cca4fd124c
Anttoni Koivu <anttoni.v.koivu+git@gmail.com>
2025-01-16 19:09:32 +0200

    Small refactor and added retrying requests when applicable

--- src/Program.cs
+++ src/Program.cs
@@ -1,127 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
+using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 using Newtonsoft.Json;
-using Microsoft.Extensions.Logging;
+using System.IO;
 
-public class Rating
+class Program
 {
-    public double rate { get; set; }
-    public int count { get; set; }
-}
+    const string OUTPUT_FILE_NAME = "grouped_products.json";
 
-public class Item
-{
-    public int id { get; set; }
-    public string? title { get; set; }
-    public double price { get; set; }
-    public string? description { get; set; }
-    public string? category { get; set; }
-    public string? image { get; set; }
-}
-
-/// <summary>
-/// Auxiliary class for handling Http requests.
-/// </summary>
-public class Program
-{
-    private readonly HttpClient _client;
-    private ILogger _logger;
-
-    public Program()
+    static async Task Main(string[] args)
     {
-        // Initialize logging for logging errors.
-        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-        ILogger logger = factory.CreateLogger("Program");
-        _logger = logger;
+        // Create the auxiliary program and retrieve content from the given URL.
+        RequestHandler handler = new RequestHandler();
+        List<Item> items = await handler.GetContent("https://fakestoreapi.com/products");
 
-        _client = new HttpClient();
-    }
-
-    /// <summary>
-    /// Sends a get request to the given URL and returns a list of items that are parsed from the received json.
-    /// </summary>
-    /// <param name="url">The URL to retrieve the content from.</param>
-    /// <returns></returns>
-    public async Task<List<Item>> GetContent(string url)
-    {
-        string content = await GetContentAsync(url);
-
-        if (content == "")
+        // Check whether or not the request was successful.
+        if (items.Count == 0)
         {
-            return new List<Item>();
+            Console.WriteLine("Unsuccessful request.");
+            return;
         }
 
-        return ParseItems(content);
-    }
+        // Group the items first by their respective categories.
+        var itemsGrouped = from item in items
+                           group item by item.category;
 
-    /// <summary>
-    /// Parse the json into a List<Item> if possible, otherwise returns an empty list.
-    /// </summary>
-    /// <param name="content">The json converted into a string.</param>
-    /// <returns></returns>
-    private List<Item> ParseItems(string content)
-    {
-        List<Item>? items = null;
+        // Form a dictionary that contains the wanted information of each item.
+        var itemsDictionary = itemsGrouped.ToDictionary(
+            group => group.Key,
+            group => group.Select(item => new { item.id, item.title, item.price }).OrderBy(item => item.price).ToArray()
+        );
+
+        // Serialize in json form.
+        string json = JsonConvert.SerializeObject(itemsDictionary, Formatting.Indented);
+        // Console.WriteLine(json);
+
+        // Finally, write the output into a new file.
         try
         {
-            items = JsonConvert.DeserializeObject<List<Item>>(content);
+            File.WriteAllText(OUTPUT_FILE_NAME, json);
+            Console.WriteLine($"Data successfully written to the file: {OUTPUT_FILE_NAME}");
         }
         catch (Exception e)
         {
-            _logger.LogError($"Failed to parse json with exception: {e.Message}");
+            Console.WriteLine($"Exception when saving the result: {e.Message}");
         }
-
-        if (items is null)
-        {
-            return new List<Item>();
-        }
-
-        return items;
-    }
-
-    /// <summary>
-    /// Sends a get request and returns the retrieved content as a string.
-    /// 
-    /// The returned string will be empty if the retrieved content is not a valid json.
-    /// </summary>
-    /// <param name="url">The URL to send the get request to.</param>
-    /// <returns></returns>
-    private async Task<string> GetContentAsync(string url)
-    {
-        _logger.LogInformation($"Sending get request to {url}");
-        HttpResponseMessage? response = null;
-
-        try
-        {
-            response = await _client.GetAsync(url);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                _logger.LogError($"Unsuccessful request, status code: {response.StatusCode}");
-                return "";
-            }
-
-            if (response.Content?.Headers?.ContentType?.MediaType != "application/json")
-            {
-                _logger.LogWarning("Request was successful but the result is not a valid json, returning empty string.");
-                return "";
-            }
-        }
-        catch (Exception e)
-        {
-            _logger.LogError($"Exception caught when sending request: {e.Message}");
-        }
-
-        if (response is null)
-        {
-            _logger.LogError($"Failed to retrieve content from url: {url}");
-            return "";
-        }
-
-        _logger.LogInformation("Request was successful.");
-        return await response.Content.ReadAsStringAsync();
     }
 }
---
+++ src/RequestHandler.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
+public class Rating
+{
+    public double rate { get; set; }
+    public int count { get; set; }
+}
+
+public class Item
+{
+    public int id { get; set; }
+    public string? title { get; set; }
+    public double price { get; set; }
+    public string? description { get; set; }
+    public string? category { get; set; }
+    public string? image { get; set; }
+}
+
+/// <summary>
+/// Auxiliary class for handling Http requests.
+/// </summary>
+public class RequestHandler
+{
+    private readonly HttpClient _client;
+    private ILogger _logger;
+
+    // Amount of time the request handler will retry before returning failure.
+    private const int MAX_RETRIES = 5;
+
+    public RequestHandler()
+    {
+        // Initialize logging for logging errors.
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger("Program");
+        _logger = logger;
+
+        _client = new HttpClient();
+        _client.Timeout = TimeSpan.FromSeconds(5);
+    }
+
+    /// <summary>
+    /// Sends a get request to the given URL and returns a list of items that are parsed from the received json.
+    /// 
+    /// If the request fails or the json is invalid, returns an empty list.
+    /// </summary>
+    /// <param name="url">The URL to retrieve the content from.</param>
+    /// <returns></returns>
+    public async Task<List<Item>> GetContent(string url)
+    {
+        string content = await GetContentAsync(url);
+
+        if (content == "")
+        {
+            return new List<Item>();
+        }
+
+        return ParseItems(content);
+    }
+
+    /// <summary>
+    /// Parse the json into a List<Item> if possible, otherwise returns an empty list.
+    /// </summary>
+    /// <param name="content">The json converted into a string.</param>
+    /// <returns></returns>
+    private List<Item> ParseItems(string content)
+    {
+        List<Item>? items = null;
+        try
+        {
+            items = JsonConvert.DeserializeObject<List<Item>>(content);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"Failed to parse json with exception: {e.Message}");
+        }
+
+        if (items is null)
+        {
+            return new List<Item>();
+        }
+
+        return items;
+    }
+    private async Task<string> GetContentAsync(string url)
+    {
+        return await GetContentAsync(url, 0);
+    }
+
+    /// <summary>
+    /// Sends a get request and returns the retrieved content as a string.
+    /// 
+    /// The returned string will be empty if the retrieved content is not a valid json.
+    /// </summary>
+    /// <param name="url">The URL to send the get request to.</param>
+    /// <param name="attempt">The number of the the current attempt.</param>
+    /// <returns></returns>
+    private async Task<string> GetContentAsync(string url, int attempt)
+    {
+        if (attempt == 0)
+        {
+            _logger.LogInformation($"Sending get request to {url}");
+        }
+        else if (attempt > MAX_RETRIES)
+        {
+            _logger.LogError("Max retry attempts exceeded, returning empty string.");
+            return "";
+        }
+        else
+        {
+            _logger.LogInformation($"Retrying the connection, attempt #{attempt}");
+        }
+
+        HttpResponseMessage? response = null;
+
+        try
+        {
+            response = await _client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogError($"Unsuccessful request, status code: {response.StatusCode}");
+                return "";
+            }
+
+            // In case of server error
+            if ((int)response.StatusCode >= 500)
+            {
+                _logger.LogInformation("Response received a server error status code, retrying..");
+                return await GetContentAsync(url, attempt + 1);
+            }
+
+            if (response.Content?.Headers?.ContentType?.MediaType != "application/json")
+            {
+                _logger.LogWarning("Request was successful but the result is not a valid json, returning empty string.");
+                return "";
+            }
+        }
+        catch (TimeoutException)
+        {
+            _logger.LogInformation("Connection was timed out, reattempting connection..");
+            return await GetContentAsync(url, attempt + 1);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"Exception caught when sending request: {e.Message}");
+        }
+
+        if (response is null)
+        {
+            _logger.LogError($"Failed to retrieve content from url: {url}");
+            return "";
+        }
+
+        _logger.LogInformation("Request was successful.");
+        return await response.Content.ReadAsStringAsync();
+    }
+}
